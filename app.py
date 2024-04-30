ac0f1ac41f0d0b27e83fe31aa7d1faf5eaf1e808
goutham <goutham3336@gmail.com>
2024-04-30 23:47:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask,url_for,request,json,Response
+app=Flask(__name__)
+
+
+@app.route("/messages",methods=["POST"])
+def api_message():
+    if request.headers["Content-Type"] == "text/plain":
+        return "Text Message" + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
+
+
+@app.route("/")
+def api_root():
+    return "Welcome"
+
+@app.route('/hello')
+def api_hello():
+    data = {
+        'hello': 'world',
+        'number': 3
+    }
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
+
+
+@app.route("/echo",methods=["GET","POST","PATCH","PUT","DELETE"])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+if __name__ == "__main__":
+    app.run()
